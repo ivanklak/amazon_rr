4ed44747eb179c17f6f6cc7637cdb8096a2f0ff4
ivanklak <ivanklak17@gmail.com>
2021-12-08 14:53:30 +0300

    refactor: add banner for advertisement

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -5,6 +5,7 @@ import {StarFill} from '@styled-icons/bootstrap/StarFill';
 import {StarHalf} from '@styled-icons/bootstrap/StarHalf';
 import {LockAlt} from '@styled-icons/boxicons-solid/LockAlt';
 
+import WideAdv from "../../app/components/Advertisment/wideAdv";
 import {addToBasket} from "../Checkout/thunks";
 import {IProduct} from "../Homepage/types";
 
@@ -76,87 +77,91 @@ const Product: FC<RouteComponentProps<IProductProps>> = props => {
     };
 
     return (
-        <div className={styles.productPage}>
-            <div className={styles.leftColumn}>
-                <div className={styles.productImgField}>
-                    <img src={currentProduct?.image} className={styles.productImg} alt="productImg"/>
+        <div>
+            <WideAdv/>
+            <div className={styles.productPage}>
+                <div className={styles.leftColumn}>
+                    <div className={styles.productImgField}>
+                        <img src={currentProduct?.image} className={styles.productImg} alt="productImg"/>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.productDescription}>
-                <h2>{currentProduct?.title}</h2>
-                <div className={styles.rating}>
-                    <div className={styles.stars}>
-                        {getStars(currentProduct?.rating.rate).map(i => (
-                            isInteger(i)
-                                ? <div key={i}><StarFill size={20} color='#F49D20FF'/></div>
-                                : <div key={i}><StarHalf size={20} color='#F49D20FF'/></div>
-                        ))}
+                <div className={styles.productDescription}>
+                    <h2>{currentProduct?.title}</h2>
+                    <div className={styles.rating}>
+                        <div className={styles.stars}>
+                            {getStars(currentProduct?.rating.rate).map(i => (
+                                isInteger(i)
+                                    ? <div key={i}><StarFill size={20} color='#F49D20FF'/></div>
+                                    : <div key={i}><StarHalf size={20} color='#F49D20FF'/></div>
+                            ))}
+                        </div>
+                        <div>{currentProduct?.rating.count} ratings</div>
                     </div>
-                    <div>{currentProduct?.rating.count} ratings</div>
-                </div>
-                <div className={styles.parameters}>
-                    <table>
-                        <tbody>
-                        <tr>
-                            <td className={styles.priceColumn}>Price:</td>
-                            <td>
-                                <span className={styles.priceNumber}>${currentProduct?.price}</span>
-                                <span className={styles.shippingPrice}> + ${shippingPrice()} shipping</span>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div className={styles.about}>
-                    <h4>About this item</h4>
-                    <ul>
-                        {descriptionFormat(currentProduct?.description).map(item => (
-                            <li key={item}>{item}</li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-            <div className={styles.productAdd}>
-                <div className={styles.addForm}>
-                    <div>
-                        <span className={styles.priceNumber}>${currentProduct?.price}</span>
-                    </div>
-                    <div>
-                        <span className={styles.shippingPrice}>${shippingPrice()} Shipping & Import Fees Deposit to Russian Federation</span>
-                    </div>
-                    <div className={styles.qtyButton}>
-                        <button onClick={onQtyClick}>Qty: {quantity}</button>
-                        {open && <DropDownCard data={qtyData} setOpen={setOpen} setQuantity={setQuantity}/>}
-                    </div>
-                    <div className={styles.addButtons}>
-                        <button onClick={onAddClick} className={styles.addButton}>Add to Cart</button>
-                        <button onClick={onBuyClick} className={styles.buyButton}>Buy now</button>
-                    </div>
-                    <div className={styles.secure}>
-                        <LockAlt size={16} color='#c2c2c2'/>
-                        <span>Secure transaction</span>
-                    </div>
-                    <div>
+                    <div className={styles.parameters}>
                         <table>
                             <tbody>
                             <tr>
-                                <td className={styles.shipItem}>Ships from</td>
-                                <td className={styles.shipInfo}>Amazon</td>
-                            </tr>
-                            <tr>
-                                <td className={styles.shipItem}>Sold by</td>
-                                <td className={styles.shipInfo}>Amazon</td>
+                                <td className={styles.priceColumn}>Price:</td>
+                                <td>
+                                    <span className={styles.priceNumber}>${currentProduct?.price}</span>
+                                    <span className={styles.shippingPrice}> + ${shippingPrice()} shipping</span>
+                                </td>
                             </tr>
                             </tbody>
                         </table>
                     </div>
-                    <div className={styles.return}>
-                        <span>Return policy: </span>
-                        <span>Returnable until Jan 31, 2022</span>
+                    <div className={styles.about}>
+                        <h4>About this item</h4>
+                        <ul>
+                            {descriptionFormat(currentProduct?.description).map(item => (
+                                <li key={item}>{item}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+                <div className={styles.productAdd}>
+                    <div className={styles.addForm}>
+                        <div>
+                            <span className={styles.priceNumber}>${currentProduct?.price}</span>
+                        </div>
+                        <div>
+                            <span className={styles.shippingPrice}>${shippingPrice()} Shipping & Import Fees Deposit to Russian Federation</span>
+                        </div>
+                        <div className={styles.qtyButton}>
+                            <button onClick={onQtyClick}>Qty: {quantity}</button>
+                            {open && <DropDownCard data={qtyData} setOpen={setOpen} setQuantity={setQuantity}/>}
+                        </div>
+                        <div className={styles.addButtons}>
+                            <button onClick={onAddClick} className={styles.addButton}>Add to Cart</button>
+                            <button onClick={onBuyClick} className={styles.buyButton}>Buy now</button>
+                        </div>
+                        <div className={styles.secure}>
+                            <LockAlt size={16} color='#c2c2c2'/>
+                            <span>Secure transaction</span>
+                        </div>
+                        <div>
+                            <table>
+                                <tbody>
+                                <tr>
+                                    <td className={styles.shipItem}>Ships from</td>
+                                    <td className={styles.shipInfo}>Amazon</td>
+                                </tr>
+                                <tr>
+                                    <td className={styles.shipItem}>Sold by</td>
+                                    <td className={styles.shipInfo}>Amazon</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className={styles.return}>
+                            <span>Return policy: </span>
+                            <span>Returnable until Jan 31, 2022</span>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+
     );
 };
 
